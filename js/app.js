8a9855479ddcac580364c041c7772c0c4fd0bedf
Maxime MIRAMOND <maxime.miramond@epitech.eu>
2015-08-18 05:12:42 +0200

    [ADD] CRT effect

--- js/app.js
+++ js/app.js
@@ -2,13 +2,27 @@
 {
     $(function()
     {
-        var touch;
-        if ($('body').width() < 992 || Modernizr.touch)
-            touch = true;
-        else
-            touch = false;
+        //var touch;
+        //if ($('body').width() < 992 || Modernizr.touch)
+        //    touch = true;
+        //else
+        //    touch = false;
 
         $('.parallax').parallax();
         $('.button-collapse').sideNav();
+
+        // jQuery to collapse the navbar on scroll
+        $(window).scroll(function()
+        {
+            if ($("nav").offset().top > 50)
+                $("nav, .navbar-fixed").addClass("notTop");
+            else
+                $("nav, .navbar-fixed").removeClass("notTop");
+        });
+        $('.button-collapse').click(function()
+        {
+            if ($('.navbar-fixed').css("z-index") < 998)
+                $('.navbar-fixed').css("z-index", 998);
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
