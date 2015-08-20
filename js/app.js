e2cb391ea4ba5fdbee7e738c625558bcccb38aa3
Maxime MIRAMOND <maxime.miramond@epitech.eu>
2015-08-20 04:45:23 +0200

    [ADD] jquery.scrollTo

--- js/app.js
+++ js/app.js
@@ -20,14 +20,19 @@
         });
 
         $("#typed-text").typed({
-            strings: ["> Bienvenue.^1000<br>" +
-            "> Je m'appelle Maxime Miramond.^1000<br>" +
-            "> J'aime le code, ^1000les films, ^1000la musique ^2000et le ^200b^200a^200c^200o^200n^200.^200"],
+            strings: ["> ^1000Bienvenue.\n" +
+            "> ^1000Je m'appelle Maxime MIRAMOND.^1000 Je suis Développeur\n" +
+            "> ^1000J'aime le code, ^1000les films, ^1000la musique ^1000et ^1000le ^1000b^200a^200c^200o^200n^200.^200 ^200:)"],
             typeSpeed: 30,
             backDelay: 500,
             loop: false,
             loopCount: false,
             showCursor: false
+        });
+        $("nav ul li a, a.brand-logo, a.page-scroll").click(function (event) {
+            $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top - $("nav").height()},
+                {duration: 1500, easing: "easeInOutExpo"});
+            event.preventDefault();
         });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
