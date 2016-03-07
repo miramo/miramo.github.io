2be40f380f1c0344a99eaf75bbf28ea0c991438a
Maxime MIRAMOND <maxime.miramond@epitech.eu>
2016-03-07 02:37:52 +0100

    [DELETE] analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,13 +25,13 @@ var b="data:image/gif;base64,R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAI
  * Created by KOALA on 08/12/2015.
  */
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-71158454-1', 'auto');
-ga('send', 'pageview');
+//(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+//
+//ga('create', 'UA-71158454-1', 'auto');
+//ga('send', 'pageview');
 
 (function($)
 {
